84c28359260b754bbb812b855158a4811caf2197
thomasfregene <fregzythomas@gmail.com>
2020-10-06 01:32:54 +0100

    Created the Dbcontext class -data layer

---
+++ SolarCoffee.Data/Models/CustomerAddress.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarCoffee.Data.Models
+{
+    class CustomerAddress
+    {
+    }
+}
---
+++ SolarCoffee.Data/SolarDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarCoffee.Data
+{
+    public class SolarDbContext : IdentityDbContext
+    {
+        public SolarDbContext() { }
+        
+        public SolarDbContext(DbContextOptions options) : base(options) { }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+
+    }
+}
