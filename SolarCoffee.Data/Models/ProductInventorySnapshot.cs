58dde9f18d841148c6497483b09c13e375401e11
thomasfregene <fregzythomas@gmail.com>
2020-10-08 03:16:52 +0100

    Added Product, Inventory and Sales Models

---
+++ SolarCoffee.Data/Models/ProductInventorySnapshot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarCoffee.Data.Models
+{
+    public class ProductInventorySnapshot
+    {
+        public int Id { get; set; }
+        public DateTime SnapShotTime { get; set; }
+        public int QuantityOnHand { get; set; }
+        public Product Product { get; set; }
+
+    }
+}
