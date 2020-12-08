3ba27248e6a057ae1b6c96406850da52de5ee022
thomasfregene <fregzythomas@gmail.com>
2020-12-08 08:43:10 +0100

    Mapping a customerModel(viewModel) to customer(data)

--- SolarCoffee.Web/Serialization/CustomerMapper.cs
+++ SolarCoffee.Web/Serialization/CustomerMapper.cs
@@ -1,11 +1,76 @@
-﻿using System;
+﻿using SolarCoffee.Data.Models;
+using SolarCoffee.Web.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SolarCoffee.Web.Serialization
 {
-    public class CustomerMapper
+    public static class CustomerMapper
     {
+        /// <summary>
+        /// serializes a customer data model into customerModel view model
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+        public static CustomerModel SerializeCustomer(Customer customer)
+        {
+            var address = new CustomerAddressModel
+            {
+                Id = customer.Id,
+                AddressLine1 = customer.PrimaryAddress.AddressLine1,
+                AddressLine2 = customer.PrimaryAddress.AddressLine2,
+                City = customer.PrimaryAddress.City,
+                State = customer.PrimaryAddress.State,
+                Country = customer.PrimaryAddress.Country,
+                PostalCode = customer.PrimaryAddress.PostalCode,
+                CreatedOn = customer.PrimaryAddress.CreatedOn,
+                UpdatedOn = customer.PrimaryAddress.UpdatedOn
+            };
+
+            return new CustomerModel
+            {
+                Id = customer.Id,
+                CreatedOn = customer.CreatedOn,
+                UpdatedOn = customer.UpdatedOn,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                PrimaryAddress = address,
+
+            };
+        }
+
+        /// <summary>
+        /// serializes a customerModel view model into customer data model 
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+        public static Customer SerializeCustomer(CustomerModel customer)
+        {
+            var address = new CustomerAddress
+            {
+                Id = customer.Id,
+                AddressLine1 = customer.PrimaryAddress.AddressLine1,
+                AddressLine2 = customer.PrimaryAddress.AddressLine2,
+                City = customer.PrimaryAddress.City,
+                State = customer.PrimaryAddress.State,
+                Country = customer.PrimaryAddress.Country,
+                PostalCode = customer.PrimaryAddress.PostalCode,
+                CreatedOn = customer.PrimaryAddress.CreatedOn,
+                UpdatedOn = customer.PrimaryAddress.UpdatedOn
+            };
+
+            return new Customer
+            {
+                Id = customer.Id,
+                CreatedOn = customer.CreatedOn,
+                UpdatedOn = customer.UpdatedOn,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                PrimaryAddress = address,
+
+            };
+        }
     }
 }
