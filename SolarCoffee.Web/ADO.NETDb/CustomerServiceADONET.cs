aabbb7d3c98a195c1b27bc5033e56aa53a95f87e
thomasfregene <fregzythomas@gmail.com>
2020-12-08 03:58:02 +0100

    Added an Order viewModel

---
+++ SolarCoffee.Web/ADO.NETDb/CustomerServiceADONET.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolarCoffee.Web.ADO.NETDb
+{
+    public class CustomerServiceADONET
+    {
+    }
+}
