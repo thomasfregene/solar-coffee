000f734601809ffb07a3ae0f5b2b6b8c506d4e5e
thomasfregene <fregzythomas@gmail.com>
2021-01-05 17:32:59 +0100

    Added static data for inventory dashboard -views folder

--- solarcoffee.frontend/src/App.vue
+++ solarcoffee.frontend/src/App.vue
@@ -51,4 +51,9 @@ export default class App extends Vue{}
 a{
   text-decoration: none;
 }
+
+hr{
+  border: 1px solid #eee;
+  margin: 1rem 0;
+}
 </style>
--- solarcoffee.frontend/src/views/Inventory.vue
+++ solarcoffee.frontend/src/views/Inventory.vue
@@ -3,17 +3,86 @@
         <h1 id="inventoryTitle">
             Inventory Dashboard
         </h1>
+        <hr>
+
+        <table id="InventoryTable" class="table">
+            <tr>
+                <th>Item</th>
+                <th>Quantity On-hand</th>
+                <th>Unit Price</th>
+                <th>Taxable</th>
+                <th>Delete</th>
+            </tr>
+
+            <tr v-for="item in inventory" :key="item.id">
+                <td>
+                    {{item.product.name}}
+                </td>
+                <td>
+                    {{item.quantityOnHand}}
+                </td>
+                <td>
+                    {{item.product.price | price}}
+                </td>
+                <td>
+                    <span v-if="item.product.isTaxable">
+                        Yes
+                    </span>
+                    <span v-else>
+                        No
+                    </span>
+                </td>
+                <td>
+                    <div>
+                        X
+                    </div>
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
-
+import{IProductInventory} from '@/types/Product';
 @Component({
     name: 'Inventory',
     components: {}
 })
-export default class Inventory extends Vue{}
+export default class Inventory extends Vue{
+    inventory: IProductInventory[] = [
+        {
+            id: 1,
+            product:{
+                id: 1,
+                name: "Some Product",
+                description: "Good Stuff",
+                price: 100,
+                createdOn: new Date(),
+                updatedOn: new Date(),
+                isTaxable: true,
+                isArchived: false
+            },
+            quantityOnHand: 100,
+            idealQuantity: 100
+        },
+          {
+            id: 2,
+            product:{
+                id: 2,
+                name: "Another Product",
+                description: "Good Stuff",
+                price: 100,
+                createdOn: new Date(),
+                updatedOn: new Date(),
+                isTaxable: false,
+                isArchived: false
+            },
+            quantityOnHand: 40,
+            idealQuantity: 20
+        }
+    ];
+}
 </script>
 
 <style lang="scss" scoped>
