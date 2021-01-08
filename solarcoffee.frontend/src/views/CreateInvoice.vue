e0277bcbe0b69a890d8d8f1a9b4287818576451f
thomasfregene <fregzythomas@gmail.com>
2021-01-08 16:03:09 +0100

    styles for the view

--- solarcoffee.frontend/src/views/CreateInvoice.vue
+++ solarcoffee.frontend/src/views/CreateInvoice.vue
@@ -71,5 +71,50 @@ const invoiceService = new InventoryService();
 </script>
 
 <style lang="scss" scoped>
-
+@import "@/scss/global.scss";
+.invoice-steps-actions {
+  display: flex;
+  width: 100%;
+}
+.invoice-step {
+}
+.invoice-step-detail {
+  margin: 1.2rem;
+}
+.invoice-order-list {
+  margin-top: 1.2rem;
+  padding: 0.8rem;
+  .line-item {
+    display: flex;
+    border-bottom: 1px dashed #ccc;
+    padding: 0.8rem;
+  }
+  .item-col {
+    flex-grow: 1;
+  }
+}
+.invoice-item-actions {
+  display: flex;
+}
+.price-pre-tax {
+  font-weight: bold;
+}
+.price-final {
+  font-weight: bold;
+  color: $solar-green;
+}
+.due {
+  font-weight: bold;
+}
+.invoice-header {
+  width: 100%;
+  margin-bottom: 1.2rem;
+}
+.invoice-logo {
+  padding-top: 1.4rem;
+  text-align: center;
+  img {
+    width: 280px;
+  }
+}
 </style>
